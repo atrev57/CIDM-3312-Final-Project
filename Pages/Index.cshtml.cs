127688b039d4d969056bc5194a2b222adc27fd0d
Alyssa Trevino <artrevino4@buffs.wtamu.edu>
2026-05-11 16:19:05 -0500

    GitHub Repo Created

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CIDM_3312_Final_Project.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
